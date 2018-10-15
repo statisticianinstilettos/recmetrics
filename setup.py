18f0ef65ca98b1af5b8bc85918baa4d76f3c0638
Claire Longo <longoclaire@gmail.com>
2018-10-15 15:45:14 -0600

    WIP

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='recommender_metrics',
+    version='0.0.0',
+    author='Claire Longo',
+    author_email='longoclaire@gmail.com',
+    packages=find_packages(),
+    description='metrics',
+)
