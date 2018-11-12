ae8f0090807eb8c489bee991a498d460404d48cf
Claire Longo <longoclaire@gmail.com>
2018-11-12 06:50:58 -0700

    personalization and ils

--- recommender_metrics/metrics.py
+++ recommender_metrics/metrics.py
@@ -23,40 +23,6 @@ def coverage(predicted, catalog):
     coverage = round(unique_predictions/(len(catalog)* 1.0)*100,2)
     return coverage
 
-def diversity(predicted):
-    """
-    Computes the diversity for a list of recommendations
-    Parameters
-    ----------
-    predicted : a list of lists
-        Ordered predictions
-        example: [['X', 'Y', 'Z'], ['X', 'Y', 'Z']]
-    Returns
-    ----------
-    coverage:
-        The diversity of the recommendations
-    """
-    diversity = 0
-    return diversity
-
-def serendipity(predicted):
-    """
-    Computes the serendipity for a list of recommendations
-    Parameters
-    ----------
-    predicted : a list of lists
-        Ordered predictions
-        example: [['X', 'Y', 'Z'], ['X', 'Y', 'Z']]
-    Returns
-    ----------
-    coverage:
-        The serendipity of the recommendations
-    """
-    serendipity = 0
-    return serendipity
-
-
-
 def ark(actual, predicted, k=10):
     """
     Computes the average recall at k.
@@ -106,3 +72,103 @@ def mark(actual, predicted, k=10):
             The mean average recall at k (mar@k)
     """
     return np.mean([ark(a,p,k) for a,p in zip(actual, predicted)])
+
+def personalization(predicted):
+    """
+    Personalization measures recommendation similarity across user's recommendations
+    using cosine similairity.
+    A high score indicates poor personalization (user's lists of recommendations are very similar).
+    A low score indicates good personalization (user's lists of recommendations are different).
+    A model is "personalizing" well if the set of recommendations for each user is different.
+    Parameters:
+    ----------
+    predicted : a list of lists
+        Ordered predictions
+        example: [['X', 'Y', 'Z'], ['X', 'Y', 'Z']]
+    Returns:
+    -------
+        The personalization score for all recommendations.
+    """
+
+    def make_rec_matrix(predicted, unique_recs):
+        rec_matrix = pd.DataFrame(index = range(len(predicted)),columns=unique_recs)
+        rec_matrix.fillna(0, inplace=True)
+        for i in rec_matrix.index:
+            rec_matrix.loc[i, predicted[i]] = 1
+        return rec_matrix
+
+    #get all unique items recommended
+    predicted_flattened = [p for sublist in predicted for p in sublist]
+    unique_recs = list(set(predicted_flattened))
+
+    #create matrix for recommendations
+    rec_matrix = make_rec_matrix(predicted, unique_recs)
+    rec_matrix_sparse = sp.csr_matrix(rec_matrix.values)
+
+    #calculate similarity for every user's recommendation list
+    similarity = cosine_similarity(X=rec_matrix_sparse, dense_output=False)
+
+    #get indicies for upper right triangle w/o diagonal
+    upper_right = np.triu_indices(similarity.shape[0], k=1)
+
+    #calculate average similarity
+    personalization = np.mean(similarity[upper_right])
+    return personalization
+
+def single_list_similarity(predicted, feature_df, k):
+    """
+    Computes the intra-list similarity at k for a single list of recommendations.
+    (NOTE: k must be greater than or equal to 2 because we need at least two
+           recommended items to calculate a similairty score between the
+           items recommended.
+    Parameters
+    ----------
+    predicted : a list
+        Ordered predictions
+        Example: ['X', 'Y', 'Z']
+    feature_df: dataframe
+        A dataframe with one hot encoded or latent features.
+        The dataframe should be indexed by the id used in the recommendations,
+        such as style_id.
+    Returns:
+    -------
+    ils_single_user: float
+        The intra-list similarity for a single list of recommendations.
+    """
+    #get features for all k recommended items
+    recs = predicted[:k]
+    recs_content = feature_df.loc[recs,]
+    recs_content = recs_content.dropna()
+    recs_content = sp.csr_matrix(recs_content.values)
+
+    #calculate similarity scores for all items in list
+    similarity = cosine_similarity(X=recs_content, dense_output=False)
+
+    #get indicies for upper right triangle w/o diagonal
+    upper_right = np.triu_indices(similarity.shape[0], k=1)
+
+    #calculate average similarity score of all recommended items in list
+    ils_single_user = np.mean(similarity[upper_right])
+    return ils_single_user
+
+def intra_list_similarity(predicted, feature_df):
+    """
+    Computes the average intra-list similarity of all recommendations.
+    This metric can be used to measure diversity of the list of recommended items.
+    Parameters
+    ----------
+    predicted : a list of lists
+        Ordered predictions
+        Example: [['X', 'Y', 'Z'], ['X', 'Y', 'Z']]
+    feature_df: dataframe
+        A dataframe with one hot encoded or latent features.
+        The dataframe should be indexed by the id used in the recommendations,
+        such as style_id.
+    Returns:
+    -------
+        The average intra-list similarity for recommendations.
+    """
+    feature_df = feature_df.fillna(0)
+    Users = range(len(predicted))
+    ils = [single_list_similarity(predicted[u], feature_df, len(predicted[u])) for u in Users]
+    return np.mean(ils)
