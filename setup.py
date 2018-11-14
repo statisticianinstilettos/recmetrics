872dae9aee4d3e129368921567f6f77c9887c84e
Claire Longo <longoclaire@gmail.com>
2018-11-14 15:18:31 -0700

    WIP

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
     packages=['recmetrics'],
     install_requires=['numpy==1.15.2',
         'pandas==0.23.4',
-        'scikit-learn==0.20.0',
-        'scipy==1.1.0',
-        'seaborn==0.9.0'],
-    version='0.1',
+        'scikit-learn',
+        'scipy',
+        'seaborn'],
+    version='0.3',
     description='Evaluation metrics for recommender systems',
 )
