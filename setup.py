0a287178b8f2ad282d256116ce2413497538cee6
Claire Longo <longoclaire@gmail.com>
2018-11-18 17:21:07 -0700

    wip

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
         'pandas',
         'scikit-learn',
         'seaborn'],
-    version='0.0.0',
+    version='0.0.1',
     description='Evaluation metrics for recommender systems',
 )
