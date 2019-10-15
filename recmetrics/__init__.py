157ab7301e4cfe46cc252e2cb01b2698c0e8f096
Kshitij Karthick <kshitij.karthick@gmail.com>
2019-10-15 16:42:50 +0200

    Fix imports of __init__.py
    Fix __init__.py import of metrics from coverage to prediction_coverage

--- recmetrics/__init__.py
+++ recmetrics/__init__.py
@@ -1,2 +1,2 @@
 from .plots import long_tail_plot, mark_plot, mapk_plot, coverage_plot, class_separation_plot, roc_plot, precision_recall_plot
-from .metrics import mark, coverage, personalization, intra_list_similarity, rmse, mse, make_confusion_matrix, recommender_precision, recommender_recall
+from .metrics import mark, prediction_coverage, personalization, intra_list_similarity, rmse, mse, make_confusion_matrix, recommender_precision, recommender_recall
