2643af34207dce09137df41a3060dbc5141a4d74
Greg Chase <9953173+gregwchase@users.noreply.github.com>
2020-10-06 22:30:45 -0600

    Additional plot test coverage

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -40,14 +40,55 @@ class TestPlots(unittest.TestCase):
         # mock_sns.set_title.assert_called_once_with("Personalization")
         
         self.assertTrue(mock_sns.barplot.called)
+    
+    @mock.patch("%s.plots.sns" % __name__)
+    def test_intra_list_similarity_plot(self, mock_sns):
+        
+        SCORES = [0.13, 0.52, 0.36]
 
-    def test_intra_list_similarity_plot(self):
+        MODEL_NAMES = ["Model A", "Model B", "Model C"]
+
+        plots.intra_list_similarity_plot(
+            intra_list_similarity_scores=SCORES,
+            model_names=MODEL_NAMES
+        )
+
+        self.assertTrue(mock_sns.barplot.called)
+    
+    # BUG: Test failing; written incorrectly
+    @mock.patch("%s.plots.sns" % __name__)
+    def test_mark_plot(self, mock_sns):
+        
+        # test_mark_scores = [[0.17, 0.25, 0.76],[0.2, 0.5, 0.74]]
+
+        # test_model_names = ['Model A', 'Model B']
+
+        # test_k_range = [1,2,3,4,5,6,7,8,9,10]
+
+        # plots.mark_plot(
+        #     mark_scores=test_mark_scores.
+        #     model_names=test_model_names,
+        #     k_range=test_k_range
+        # )
+
+        # self.assertTrue(mock_sns.show.called)
         pass
+    
+    # BUG: Test failing; written incorrectly
+    @mock.patch("%s.plots.sns" % __name__)
+    def test_mapk_plot(self, mock_sns):
+        
+        # MAPK_SCORES = [[0.17, 0.25, 0.76],[0.2, 0.5, 0.74]]
 
-    def test_mark_plot(self):
-        pass
+        # MODEL_NAMES = ["Model A", "Model B"]
 
-    def test_mapk_plot(self):
+        # K_RANGE = [[1,2,3],[4,5,6]]
+
+        # plots.mapk_plot(mapk_scores=MAPK_SCORES,
+        #     model_names=MODEL_NAMES,
+        #     k_range=K_RANGE)
+
+        # self.assertTrue(mock_sns.lineplot.called)
         pass
 
     def test_class_separation_plot(self):
