fc667bd8bff6333f5f075a61c303acd7b7319c5c
Greg Chase <9953173+gregwchase@users.noreply.github.com>
2020-10-13 22:00:34 -0600

    Add given/when/then comments

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -6,67 +6,103 @@ from unittest import mock
 
 class TestMetrics(unittest.TestCase):
 
-    # def __init__(self, *args, **kwargs):
-    #     super(TestMetrics, self).__init__(*args, **kwargs)
-    #     self.recsys = Metrics()
-
     # BUG: Test failing
     def test_novelty(self):
         """
+        Test novelty function
         """
+
+        # GIVEN test_novelty metrics
+        test_predicted = [['X', 'Y', 'Z'], ['X', 'Y', 'Z']]
+        test_pop = {1198: 893, 1270: 876, 593: 876, 2762: 867}
+        test_u = 5
+        test_n = 3
+
+        # WHEN metrics.novelty is run
         novelty_score = metrics.novelty(
-            predicted = [['X', 'Y', 'Z'], ['X', 'Y', 'Z']],
-            pop = {1198: 893, 1270: 876, 593: 876, 2762: 867},
-            u = 5,
-            n = 3
+            predicted = test_predicted,
+            pop = test_pop,
+            u = test_u,
+            n = test_n
         )
 
+        # THEN the novelty score should equal the expected tuple
         self.assertEqual(novelty_score, (2, 3))
 
     def test_prediction_coverage(self):
         """
+        Test prediction_coverage function
         """
-        prediction_coverage = metrics.prediction_coverage(
-            predicted = [['X', 'Y', 'Z'], ['X', 'Y', 'Z']],
-            catalog=['A', 'B', 'C', 'X', 'Y', 'Z'])
 
+        # GIVEN predictions and coverage lists
+        test_predicted = [['X', 'Y', 'Z'], ['X', 'Y', 'Z']]
+        test_catalog = ['A', 'B', 'C', 'X', 'Y', 'Z']
+
+        # WHEN metrics.prediction_coverage is run
+        prediction_coverage = metrics.prediction_coverage(
+            predicted = test_predicted,
+            catalog = test_catalog)
+
+        # THEN the prediction coverage should equal an expected value of 50
         self.assertEqual(prediction_coverage, 50.)
 
     def test_catalog_coverage(self):
         """
+        Test catalog_coverage function
         """
+
+        # GIVEN test catalog_coverage metrics
+        test_predicted = [['X', 'Y', 'Z'], ['X', 'Y', 'Z']]
+        test_catalog = ['A', 'B', 'C', 'X', 'Y', 'Z']
+        test_k = 3
+
+        # WHEN metrics.catalog_coverage is run
         catalog_coverage = metrics.catalog_coverage(
-            predicted = [['X', 'Y', 'Z'], ['X', 'Y', 'Z']],
-            catalog=['A', 'B', 'C', 'X', 'Y', 'Z'],
-            k=3        
+            predicted = test_predicted,
+            catalog = test_catalog,
+            k = test_k
         )
 
+        # THEN the coverage should equal 50.
         self.assertEqual(catalog_coverage, 50.)
 
     def test_mark(self):
         """
+        Test mean absolute recall @ k (MAPK) function
         """
+
+        # GIVEN test MAR@K metrics
+        test_actual = [['A', 'B', 'X'], ['A', 'B', 'Y']]
+        test_predicted = [['X', 'Y', 'Z'], ['X', 'Y', 'Z']]
+        test_k = 5
+
+        # WHEN metrics.mark is run
         mean_abs_recall_k = metrics.mark(
-            actual=[['A', 'B', 'X'], ['A', 'B', 'Y']],
-            predicted=[['X', 'Y', 'Z'], ['X', 'Y', 'Z']],
-            k=5
+            actual=test_actual,
+            predicted=test_predicted,
+            k=test_k
         )
 
+        # THEN the mean absolute recall @ k should equal the expected value
         self.assertEqual(mean_abs_recall_k, 0.25)
 
     def test_personalization(self):
         """
+        Test personalization function
         """
 
-        example_predictions = [
+        # GIVEN a list of lists of predictions
+        test_predictions = [
             ['1', '2', 'C', 'D'],
             ['4', '3', 'm', 'X'],
             ['7', 'B', 't', 'X']
         ]
         
+        # WHEN metrics.personalization is run
         personalization_score = metrics.personalization(
-            predicted=example_predictions)
+            predicted=test_predictions)
 
+        # THEN the personalization score should be within the expected value
         self.assertAlmostEqual(personalization_score, 0.916, places=2)
 
     def test_intra_list_similarity(self):
@@ -76,59 +112,85 @@ class TestMetrics(unittest.TestCase):
 
     def test_mse(self):
         """
+        Test mean squared error (MSE) function
         """
 
+        # GIVEN predictions and actual values
         y_pred = [1.5, 3.2, 1.4, 3.3, 1.7, 3.8, 4.4, 1.6, 1.2, 3.7]
-        
         y_test = [2, 4, 3, 1, 3, 4, 5, 2, 1, 4]
 
+        # WHEN metrics.mse is run
         mse = metrics.mse(y=y_test, yhat=y_pred)
 
+        # THEN the expected MSE should be 1.11 within two decimal places
         self.assertAlmostEqual(mse, 1.11, places=2)
 
     def test_rmse(self):
         """
+        Test root mean square error (RMSE) function
         """
         
+        # GIVEN predictions and actual values
         y_pred = [1.5, 3.2, 1.4, 3.3, 1.7, 3.8, 4.4, 1.6, 1.2, 3.7]
-        
         y_test = [2, 4, 3, 1, 3, 4, 5, 2, 1, 4]
 
+        # WHEN metrics.rmse is run
         rmse = metrics.rmse(y=y_test, yhat=y_pred)
 
+        # THEN the expected RMSE should be 1.054 within two decimal places
         self.assertAlmostEqual(rmse, 1.054, places=2)
 
     # TODO: Additional test coverage for confusion matrix
     @mock.patch("%s.metrics.plt" % __name__)
     def test_make_confusion_matrix(self, mock_plt):
         """
+        Test make_confusion_matrix function
         """
         
+        # GIVEN predictions and actual values
         y_pred = [1, 3, 1, 3, 1, 3, 4, 1, 1, 3]
-        
         y_test = [2, 4, 3, 1, 3, 4, 5, 2, 1, 4]
 
+        # WHEN metrics.make_confusion_matrix is run
         metrics.make_confusion_matrix(
             y = y_test,
             yhat = y_pred
         )
 
+        # THEN plt.title() and plt.show() should be called in the function
         self.assertTrue(mock_plt.title.called)
+        self.assertTrue(mock_plt.show.called)
 
     def test_recommender_precision(self):
         """
+        Test recommender_precision function
         """
-        recommender_precision = metrics.recommender_precision(
-            predicted = [['X', 'Y', 'Z'], ['X', 'Y', 'Z']],
-            actual=['A', 'B', 'C', 'X', 'Y', 'Z'])
 
+        # GIVEN predictions and actual values
+        test_predicted = [['X', 'Y', 'Z'], ['X', 'Y', 'Z']]
+        test_actual = ['A', 'B', 'C', 'X', 'Y', 'Z']
+        
+        # WHEN metrics.recommender_precision is run
+        recommender_precision = metrics.recommender_precision(
+            predicted = test_predicted,
+            actual = test_actual)
+
+        # THEN the expected value should equal 0
         self.assertEqual(recommender_precision, 0.)
 
     def test_recommender_recall(self):
         """
+        Test recommender_recall function
         """
-        recommender_recall = metrics.recommender_recall(
-            predicted = [['X', 'Y', 'Z'], ['X', 'Y', 'Z']],
-            actual=['A', 'B', 'C', 'X', 'Y', 'Z'])
 
+        # GIVEN predictions and actual values
+        test_predicted = [['X', 'Y', 'Z'], ['X', 'Y', 'Z']]
+        test_actual = ['A', 'B', 'C', 'X', 'Y', 'Z']
+
+        # WHEN metrics.recommender_recall is run
+        recommender_recall = metrics.recommender_recall(
+            predicted = test_predicted,
+            actual= test_actual)
+
+        # THEN the expected value should equal 0
         self.assertEqual(recommender_recall, 0.)
